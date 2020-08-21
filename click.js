0fb69b69173e80685316df0dc40ec785b2d1d11e
Bhanjo <hanjo1515@naver.com>
2020-08-21 14:57:43 +0900

    0821_1457

--- click.js
+++ click.js
@@ -1,1 +1,14 @@
-const login =
+const logo = document.getElementById(".jsGoHoem");
+
+function myHome() { //로고클릭 이벤트 처리
+    const openHome = window.open("index.html");
+}
+
+function clickLogo() { //로고클릭 이벤트
+    logo.addEventListener("click", myHome);
+}
+
+function init() {
+    clickLogo();
+}
+init();
